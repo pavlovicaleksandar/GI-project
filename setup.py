0c3a1b6fa926c68b75617e956a4bf050947bc026
Aleksandar Pavlovic <aleksandar.pavlovic@sbgenomics.com>
2021-05-16 19:36:33 +0200

    Stuff for pypi

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+import setuptools
+setup(
+    name='giseed',
+    packages=['genomics'],
+    py_modules=['genomics.main'],
+    version='1.0.0',
+    license='MIT',
+    description='Tool developed for GI course on ETF Belgrade',
+    author='Marko Aleksandar Marko',
+    author_email='',
+    url='https://github.com/accchi/GI-project',
+    keywords=['bwt', 'genomics', 'seed&extend'],
+    install_requires=[
+      'biopython==1.78',
+      'numpy==1.20.3',
+      'pytest==6.2.4',
+      'click==8.0.0'
+    ],
+    entry_points={
+      'console_scripts': [
+        'giseed=genomics.main:cli'
+      ]
+    }
+)
