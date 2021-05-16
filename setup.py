7e3cf461f5608f12578604d25b8cf6bf575f59c4
Aleksandar Pavlovic <aleksandar.pavlovic@sbgenomics.com>
2021-05-16 23:00:26 +0200

    Update version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='giseed',
     packages=['genomics'],
     py_modules=['genomics.main'],
-    version='1.0.4',
+    version='1.0.5',
     license='MIT',
     description='Tool developed for GI course on ETF Belgrade',
     author='Marko Aleksandar Marko',
