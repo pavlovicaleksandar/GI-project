1e3041ae87ab26b5e7885efba09ab9da9fda6487
Marko Milicevic <39123062+milicevicMarko@users.noreply.github.com>
2021-05-21 12:09:38 +0200

    Code for plotting
    Please check requirements.txt
    Open - how to import and read sam files
    Open - how to import and read txt files

--- genomics/plot_results.py
+++ genomics/plot_results.py
@@ -26,18 +26,36 @@ def import_all_giska_outputs():
     return result
 
 
-def draw_plot(title, legend, read_alignment_scores):
+def draw_single_plot(graph_num, pars, read):
     # x - reads, y - alignment score
-    # title
-    # legend - margin, seed, etv
     plt.figure()
-    # plt.legend(legend) todo
-    plt.suptitle(title)
+    plt.suptitle(f'Graph number: {graph_num}')
     plt.xlabel("Reads")
     plt.ylabel("Alignment Score")
-    i = range(len(read_alignment_scores))
-    plt.plot(i, read_alignment_scores)
+    plt.figtext(0.5, .8,
+                pars,
+                horizontalalignment="center",
+                wrap=True, fontsize=10,
+                bbox={'facecolor': 'yellow',
+                      'alpha': 0.3, 'pad': 5})
+    plt.plot(range(len(read)), read)
     plt.show()
 
 
-draw_plot("proba", "margin: 2", [1, 2, 3, 4, 5, 9])
+def draw_all_plot(reads):
+    # x - reads, y - alignment score
+    # label - match, mismatch, gap, margin, seed, etc
+    plt.figure()
+    plt.suptitle("All in one")
+    plt.xlabel("Reads")
+    plt.ylabel("Alignment Score")
+    for r in range(len(reads)):
+        plt.plot(range(len(reads[r])), reads[r], label=r)
+    plt.legend()
+    plt.show()
+
+
+# dummy test data
+reads = [[7, 2, 5, 3, 5, 9], [1, 2, 3, 4, 5, 9], [11, 5, 7, 1, 0, 3]]
+draw_single_plot(1, "match: 2; mismatch: -2; gap: -5", reads[0])
+draw_all_plot(reads)
