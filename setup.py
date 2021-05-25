3cfd57be23ca268ce1a2dffd2d2c0e602fc80167
Marko Milicevic <markomilicevic97@gmail.com>
2021-05-25 08:52:11 +0200

    Version Update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='giseed',
     packages=['genomics'],
     py_modules=['genomics.main'],
-    version='1.3.0',
+    version='1.3.1',
     license='MIT',
     description='Tool developed for GI course on ETF Belgrade',
     author='Marko Aleksandar Marko',
